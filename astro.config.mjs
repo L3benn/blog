167f96fb4ef7098311ed5a941ff41bd53a71e207
root <admin@teamwill.local>
2026-07-29 20:24:40 +0100

    clean up: remove borders, darken theme, Roboto Condensed font, wider content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,9 @@
-// @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	// Change to your GitHub Pages URL
-	// User site: https://l3benn.github.io (with base: '/')
-	// Project site: https://l3benn.github.io/blog (with base: '/blog')
 	site: 'https://l3benn.github.io',
 	base: '/',
 	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
 });
